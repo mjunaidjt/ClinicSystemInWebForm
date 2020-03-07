2cc1194deaf5343e3b74082c20b0269bff2d6825
Muhammad Junaid <junaidt07@gmail.com>
2020-03-07 22:32:06 +0500

    Created Web Form Project with name ClinicSystemInWebForm

---
+++ ClinicSystemInWebForm/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ClinicSystemInWebForm.Startup))]
+namespace ClinicSystemInWebForm
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
